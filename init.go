55f917db1cbdb20fcf88e6507867171d8127f038
kostya <konstantin.nizhinskij@privatbank.ua>
2018-01-21 14:40:23 +0200

    version 0.0.2

--- init.go
+++ init.go
@@ -3,6 +3,7 @@ package i18n
 import (
 	"github.com/jackc/pgx"
 	"os"
+	"strconv"
 )
 
 var typeStorage string
@@ -15,24 +16,28 @@ func init(){
 	typeStorage=getOS("I18N_TYPE_STORAGE","sql_pg")
 	cookieName=getOS("I18N_COOKIE_NAME","cc_lang")
 	langDefault=getOS("I18N_DEFAULT","en")
-	if(typeStorage=="sql_pg"){
-		var err error
-		connPoolConfig := pgx.ConnPoolConfig{
-			ConnConfig: pgx.ConnConfig{
-				Host:     getOS("I18N_SQL_HOST","localhost"),
-				User:     getOS("I18N_SQL_USER","postgres"),
-				Password: getOS("I18N_SQL_PASSWORD","qwerty"),
-				Database: getOS("I18N_SQL_DATABASE","cc_data"),
-			},
-			MaxConnections: 5,
-		}
-		dbPool, err = pgx.NewConnPool(connPoolConfig)
-		if err != nil {
-			panic(err)
-			os.Exit(1)
-		}
+}
+func Connect(host string,port string, user string, password string,database string) {
+	var err error
+	port_int, err := strconv.ParseUint(port, 10, 16)
+	if(err!=nil){
+		panic(err)
 	}
-
+	connPoolConfig := pgx.ConnPoolConfig{
+		ConnConfig: pgx.ConnConfig{
+			Host:     host,
+			Port:     uint16(port_int),
+			User:     user,
+			Password: password,
+			Database: database,
+		},
+		MaxConnections: 5,
+	}
+	dbPool, err = pgx.NewConnPool(connPoolConfig)
+	if err != nil {
+		panic(err)
+		os.Exit(1)
+	}
 }
 func getOS(keyos string,def string) string {
 	value,err:=os.LookupEnv(keyos)
